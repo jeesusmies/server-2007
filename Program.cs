323b66ce5084cda92c7d1a38a027ff2acb2825c7
jeesusmies <46649783+jeesusmies@users.noreply.github.com>
2020-10-16 07:24:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using server_2007.http;
+using server_2007.mysql;
+using System;
+
+namespace server_2007
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpServer server = new HttpServer("http://127.0.0.1:80/", new SqlInit("", "", "", ""));
+        }
+    }
+}
